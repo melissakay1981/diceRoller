e567c348d2b97e3d4fd4a974c89988d82367514f
melissa <melissakay1981@yahoo.com>
2020-05-18 14:43:45 -0500

    2nd commit

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const total = document.querySelector ('#total')
 const rollDice = document.querySelector("#roll")
 const allRolls = document.querySelector('#Show')
 const list = document.querySelector('#list')
-
+const diceSound = new Audio("www.gnubg.org/win32/wavs/roll.wav")
+const resetButton = document.querySelector("#reset-button");
+let reset = 0
 let diceRolls = []
 let counter=0
 let totalSum = 0
 
 rollDice.addEventListener('click',function(){
  let playerInput = numberOfRolls.value
-
+diceSound.play()
     while(playerInput>counter){
       let random =Math.floor(Math.random()*6)+1;
    diceRolls.push(random) 
@@ -34,12 +36,21 @@ total.innerHTML ="Total "+ totalSum
 
          diceIndex+=1
        }
+       diceIndex =0
+      })
 
-    })
+       resetButton.addEventListener("click", function(){
+         console.log("reset");
+        counter = 0
+        totalSum = 0
+        total.innerHTML = ""
+        list.innerHTML = ""
+        diceRolls.splice (0,diceRolls.length)
+         numberOfRolls.value = 0
+  
 
 
-
-//System.out.println("Sum:"+sum);
+   })
 
 
     //let newCounterValue = Number(counter.innerHTML) + 1;
